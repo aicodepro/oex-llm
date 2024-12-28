9b8d091964e7d50b3a84170996193f4a2b64cc12
devendra yadav <devendra@crunchyfashion.com>
2024-12-28 22:02:09 +0530

    AicodeProProductService - Documents

---
+++ packages/components/nodes/documentloaders/AiCodeProProductsAPI/AiCodeProProductsAPI.ts
@@ -0,0 +1,186 @@
+import { omit } from 'lodash'
+import { ICommonObject, IDocument, INode, INodeData, INodeParams } from '../../../src/Interface'
+import { TextSplitter } from 'langchain/text_splitter'
+import { AiCodeProProducts } from './aicodepro_products'
+import { getCredentialData, getCredentialParam } from '../../../src/utils'
+import { Document } from '@langchain/core/documents'
+
+class AiCodeProProductsAPI_DocumentLoaders implements INode {
+    label: string
+    name: string
+    version: number
+    description: string
+    type: string
+    icon: string
+    category: string
+    baseClasses: string[]
+    credential: INodeParams
+    inputs: INodeParams[]
+
+    constructor() {
+        this.label = 'Products API Document Loader'
+        this.name = 'aiCodeProProductsAPILoader'
+        this.version = 1.0
+        this.type = 'Document'
+        this.icon = 'aicodepro.svg'
+        // DO Not change the name Category of Document Loaders
+        this.category = 'Document Loaders'
+        this.description = 'Load and process data from AiCodePro Products results'
+        this.baseClasses = [this.type]
+        this.credential = {
+            label: 'Connect Credential',
+            name: 'credential',
+            type: 'credential',
+            optional: false,
+            credentialNames: ['aicodeproCredentails']
+        }
+        this.inputs = [
+            {
+                label: 'Website Domain',
+                name: 'websiteDomain',
+                type: 'string'
+            },
+            {
+                label: 'Text Splitter',
+                name: 'textSplitter',
+                type: 'TextSplitter',
+                optional: true
+            },
+            {
+                label: 'Additional Metadata',
+                name: 'metadata',
+                type: 'json',
+                description: 'Additional metadata to be added to the extracted documents',
+                optional: true,
+                additionalParams: true
+            },
+            {
+                label: 'Omit Metadata Keys',
+                name: 'omitMetadataKeys',
+                type: 'string',
+                rows: 4,
+                description:
+                    'Each document loader comes with a default set of metadata keys that are extracted from the document. You can use this field to omit some of the default metadata keys. The value should be a list of keys, separated by comma. Use * to omit all metadata keys except the ones you specify in the Additional Metadata field',
+                placeholder: 'key1, key2, key3.nestedKey1',
+                optional: true,
+                additionalParams: true
+            }
+        ]
+    }
+
+    async init(nodeData: INodeData, _: string, options: ICommonObject): Promise<any> {
+        const textSplitter = nodeData.inputs?.textSplitter as TextSplitter
+        const websiteDomain = nodeData.inputs?.websiteDomain as string
+        const metadata = nodeData.inputs?.metadata
+        const _omitMetadataKeys = nodeData.inputs?.omitMetadataKeys as string
+
+        let omitMetadataKeys: string[] = []
+        if (_omitMetadataKeys) {
+            omitMetadataKeys = _omitMetadataKeys.split(',').map((key) => key.trim())
+        }
+
+        const credentialData = await getCredentialData(nodeData.credential ?? '', options)
+        const aicodeproToken = getCredentialParam('token', credentialData, nodeData)
+        const tenantId = getCredentialParam('tenantId', credentialData, nodeData)
+        const storeId = getCredentialParam('storeId', credentialData, nodeData)
+        let domain = getCredentialParam('domain', credentialData, nodeData)
+        if (domain === undefined || domain === '') {
+            domain = 'https://api.aicodepro.com'
+        }
+        if (!aicodeproToken || !tenantId || !storeId) {
+            throw new Error('Missing required credentials: token, tenantId, or storeId.')
+        }
+        const loader = new AiCodeProProducts({ aicodeproToken: aicodeproToken, tenantId: tenantId, storeId: storeId, domain: domain })
+
+        let docs: IDocument[] = []
+        let currentPage = 1
+        let hasNextPage = true
+        while (hasNextPage) {
+            let searchResults
+            try {
+                searchResults = await loader._call(currentPage)
+            } catch (error) {
+                throw new Error(`Failed to fetch product data: ${error.message}`)
+            }
+
+            const parsedResults = JSON.parse(searchResults)
+            if (!parsedResults.data || !Array.isArray(parsedResults.data)) {
+                throw new Error('Invalid API response format: "data" field is missing or not an array.')
+            }
+            const pagination = parsedResults.meta?.pagination
+            docs.push(
+                ...parsedResults.data.map(
+                    (result: any) =>
+                        new Document({
+                            pageContent: result.attributes?.bodyHtml || '',
+                            metadata: {
+                                title: result.attributes?.title || 'Untitled',
+                                vendor: result.attributes?.vendor,
+                                status: result.attributes?.status,
+                                tags: result.attributes?.tags,
+                                image: result.attributes?.image?.src,
+                                price: result.attributes?.price,
+                                color: result.attributes?.color,
+                                parentSku: result.attributes?.parentSku,
+                                primaryKeywords: result.attributes?.primaryKeywords,
+                                metaTitle: result.attributes?.metaTitle,
+                                metaDescription: result.attributes?.metaDescription,
+                                secondaryKeywords: result.attributes?.secondaryKeywords,
+                                productType: result.attributes?.productType,
+                                quantity: result.attributes?.quantity,
+                                mrp: result.attributes?.mrp,
+                                size: result.attributes?.size,
+                                link: result.attributes?.handle ? `${websiteDomain}/products/${result.attributes.handle}` : null
+                            }
+                        })
+                )
+            )
+            // Check for pagination end
+            hasNextPage = pagination?.pageCount > currentPage
+            currentPage++
+        }
+        //console.log(docs)
+        docs = docs.filter((doc) => doc.pageContent && doc.pageContent.trim())
+        // Remove empty docs
+
+        if (textSplitter) {
+            docs = await textSplitter.splitDocuments(docs)
+        }
+
+        if (metadata) {
+            const parsedMetadata = typeof metadata === 'object' ? metadata : JSON.parse(metadata)
+            docs = docs.map((doc) => ({
+                ...doc,
+                metadata:
+                    _omitMetadataKeys === '*'
+                        ? {
+                              ...parsedMetadata
+                          }
+                        : omit(
+                              {
+                                  ...doc.metadata,
+                                  ...parsedMetadata
+                              },
+                              omitMetadataKeys
+                          )
+            }))
+        } else {
+            docs = docs.map((doc) => ({
+                ...doc,
+                metadata:
+                    _omitMetadataKeys === '*'
+                        ? {}
+                        : omit(
+                              {
+                                  ...doc.metadata
+                              },
+                              omitMetadataKeys
+                          )
+            }))
+        }
+
+        return docs
+    }
+}
+
+module.exports = { nodeClass: AiCodeProProductsAPI_DocumentLoaders }
---
+++ packages/components/nodes/documentloaders/AiCodeProProductsAPI/aicodepro_products.ts
@@ -0,0 +1,91 @@
+import { getEnvironmentVariable } from '@langchain/core/utils/env'
+import { Tool } from '@langchain/core/tools'
+
+/**
+ * Interface for the parameters required to instantiate a AiCodePro Products
+ * instance.
+ */
+export interface AiCodeProProductsParams {
+    aicodeproToken?: string
+    storeId?: string
+    tenantId?: string
+    domain?: string
+    pageSize?: number
+}
+
+/**
+ * Class for interacting with the Aicodepro PIM. It extends the Tool
+ * class and requires an API key to function. The API key can be passed in
+ * during instantiation or set as an environment variable named
+ * 'BRAVE_SEARCH_API_KEY'.
+ */
+export class AiCodeProProducts extends Tool {
+    static lc_name() {
+        return 'AiCodeProProducts'
+    }
+
+    name = 'aicodepro-products'
+
+    description =
+        'Product Information Management. useful for when you need to answer questions about products. input should be a search query.'
+
+    aicodeproToken: string
+    storeId?: string
+    tenantId?: string
+    domain?: string
+    pageSize?: number
+
+    constructor(
+        fields: AiCodeProProductsParams = {
+            aicodeproToken: getEnvironmentVariable('AICODEPRO_PRODUCTS_API_KEY')
+        }
+    ) {
+        super()
+
+        if (!fields.aicodeproToken) {
+            throw new Error(
+                `AiCodePro Products API token not set. Please pass it in or set it as an environment variable named "AICODEPRO_PRODUCTS_API_KEY".`
+            )
+        }
+        if (!fields.storeId) {
+            throw new Error('storeId is required. Please provide it as a parameter.')
+        }
+
+        if (!fields.tenantId) {
+            throw new Error('tenantId is required. Please provide it as a parameter.')
+        }
+
+        this.aicodeproToken = fields.aicodeproToken
+        this.storeId = fields.storeId
+        this.tenantId = fields.tenantId
+        this.domain = fields.domain
+            ? fields.domain.endsWith('/')
+                ? fields.domain.slice(0, -1)
+                : fields.domain
+            : 'https://api.aicodepro.com'
+        this.pageSize = fields.pageSize && fields.pageSize > 0 ? fields.pageSize : 10
+    }
+
+    /** @ignore */
+    async _call(currentPage: number = 1): Promise<string> {
+        if (typeof currentPage !== 'number' || currentPage < 1) {
+            throw new Error('currentPage must be a positive number.')
+        }
+        const headers = {
+            Authorization: `Bearer ${this.aicodeproToken}`,
+            Accept: 'application/json'
+        }
+
+        const url = `${this.domain}/api/product-service/${this.tenantId}/${this.storeId}/channel-products?pagination[page]=${currentPage}&pagination[pageSize]=${this.pageSize}&filters[isListedOnShopify][$eq]=true&locale=en`
+        console.log('Fetching products data...', url)
+        const response = await fetch(url, {
+            method: 'GET',
+            headers
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error ${response.status}`)
+        }
+        const parsedResponse = await response.json()
+        return JSON.stringify(parsedResponse)
+    }
+}
